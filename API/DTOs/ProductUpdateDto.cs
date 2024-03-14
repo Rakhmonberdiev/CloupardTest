a331cb635bf5e1332a003bffc2e7a2b1d228de4b
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2024-03-14 19:59:37 +0500

    DTOs

---
+++ API/DTOs/ProductUpdateDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class ProductUpdateDto
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
