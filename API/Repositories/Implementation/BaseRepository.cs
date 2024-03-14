1aeaf9cf53ab0e33c7c27b5173c056b18c600326
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2024-03-14 22:47:21 +0500

    Base CRUD

--- API/Repositories/Implementation/BaseRepository.cs
+++ API/Repositories/Implementation/BaseRepository.cs
@@ -1,6 +1,7 @@
 ﻿using API.Data;
 using API.Repositories.Interface;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace API.Repositories.Implementation
 {
@@ -19,9 +20,42 @@ namespace API.Repositories.Implementation
             await SaveAsync();
         }
 
+        public async Task DeleteAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+            await SaveAsync();
+        }
+
+        public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null)
+        {
+            IQueryable<T> entity = _dbSet;
+            if(filter != null)
+            {
+                entity = entity.Where(filter);
+            }
+            return await entity.ToListAsync();
+        }
+
+        public async Task<T> GetAsync(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> entity = _dbSet;
+      
+            if (filter != null)
+            {
+                entity = entity.Where(filter);
+            }
+            return await entity.FirstOrDefaultAsync();
+        }
+
         public async Task SaveAsync()
         {
             await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(T entity)
+        {
+            _dbSet.Update(entity);
+            await SaveAsync();
         }
     }
 }
--- API/Repositories/Interface/IBaseRepository.cs
+++ API/Repositories/Interface/IBaseRepository.cs
@@ -1,7 +1,13 @@
-﻿namespace API.Repositories.Interface
+﻿using System.Linq.Expressions;
+
+namespace API.Repositories.Interface
 {
     public interface IBaseRepository<T> where T : class
     {
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter=null);
+        Task<T> GetAsync(Expression<Func<T, bool>> filter);
+        Task Update(T entity);
+        Task DeleteAsync(T entity);
         Task CreateAsync(T entity);
         Task SaveAsync();
     }
