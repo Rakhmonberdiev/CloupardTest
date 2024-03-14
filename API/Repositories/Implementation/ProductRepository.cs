b95b867dfe9fd43a522a98d7e1cc7106b7362911
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2024-03-14 20:34:22 +0500

    ProductRepository

---
+++ API/Repositories/Implementation/ProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using API.Data;
+using API.Models;
+using API.Repositories.Interface;
+
+namespace API.Repositories.Implementation
+{
+    public class ProductRepository : BaseRepository<Product>, IProductRepository
+    {
+        public ProductRepository(AppDbContext db) : base(db)
+        {
+        }
+    }
+}
