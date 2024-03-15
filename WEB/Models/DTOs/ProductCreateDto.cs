90f509fe2ff7806b9624ec4baea580310497f8cb
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2024-03-15 13:28:50 +0500

    DTOs for update, create

---
+++ WEB/Models/DTOs/ProductCreateDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WEB.Models.DTOs
+{
+    public class ProductCreateDto
+    {
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
